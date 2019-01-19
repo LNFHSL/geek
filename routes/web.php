3797334cccb83e82ee13088571ff1b16c0d7cb1f
Your Name <544715883@qq.com>
2019-01-19 10:23:48 +0800

    登录取消mock改为本地

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,10 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
-
+*/ 
+Route::post('user/login','Admin\Manager@login');
+Route::post('user/info','Admin\Manager@info');
+Route::post('user/logout','Admin\Manager@logout');
 Route::get('/', function () {
     return view('c');
 });
